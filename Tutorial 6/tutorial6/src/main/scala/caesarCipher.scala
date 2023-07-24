676a2a32086cd7c118aa2a40def035623dd2fd45
samudiperera09 <samudiperera125@gmail.com>
2023-07-24 21:38:08 +0530

    Question 1 Tutorial 6

--- Tutorial 6/tutorial6/src/main/scala/caesarCipher.scala
+++ Tutorial 6/tutorial6/src/main/scala/caesarCipher.scala
@@ -18,16 +18,25 @@ object caesarCipher {
   def cipher(str: String, key: Int, operation: (String,Int) => String): String = {
     operation(str,key)
   }
+
   def main(args : Array[String]) : Unit = {
-    val str = "Caesar"
-    val key = 1
+    print("Enter the string ")
+    val str = StdIn.readLine()
+    print("Enter the key ")
+    val key = StdIn.readInt()
 
-    val encryptedStr = cipher(str,key,encrypt)
-    val decryptedStr = cipher(encryptedStr, key, decrypt)
+    print("If you want to encrypt, enter 1 and if you want to decrypt, enter 2 ")
+    val doCase = StdIn.readInt()
 
-    println("Original Text: " + str)
-    println("Encrypted Text: " + encryptedStr)
-    println("Decrypted Text: " + decryptedStr)
+    if(doCase == 1){
+      println("The encrypted text is " + cipher(str,key,encrypt))
+    }
+    else if(doCase == 2){
+      println("The decrypted text is " + cipher(str,key,decrypt))
+    }
+    else{
+      println("Please enter 1 or 2")
+    }
   }
 
 }
